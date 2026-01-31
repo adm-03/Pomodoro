168cd60d41aa5aaa16de7aa920967260e8a65868
adm-03 <globov.motya@mail.ru>
2026-01-31 18:15:08 +0700

    add db&cahce values to setting

--- settings.py
+++ settings.py
@@ -1,5 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    sqlite_db_name: str = 'pomodoro.sqlite'
+    DB_HOST: str = 'localhost'
+    DB_PORT: int = 5433
+    DB_USER: str = 'postrges'
+    DB_PASSWORD: str = 'password'
+    DB_NAME: str = 'pomodoro'
 
+    CACHE_HOST: str = 'localhost'
+    CACHE_PORT: int = 6379
+    CAHCE_DB: int = 0
